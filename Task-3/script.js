70e48ef37eef3c52425926e3d28e60413ed45b4e
kajal shukla <shuklakajal1703@gmail.com>
2025-08-04 23:03:44 +0530

    Add files via upload

---
+++ Task-3/script.js
@@ -0,0 +1,33 @@
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
+
+function updateDisplay() {
+  const time = elapsedTime;
+  const minutes = Math.floor(time / 60000);
+  const seconds = Math.floor((time % 60000) / 1000);
+  const milliseconds = time % 1000;
+
+  document.getElementById("display").textContent =
+    `${String(minutes).padStart(2, '0')}:` +
+    `${String(seconds).padStart(2, '0')}:` +
+    `${String(milliseconds).padStart(3, '0')}`;
+}
+
+function startTimer() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    updateDisplay();
+  }, 10);
+}
+
+function pauseTimer() {
+  clearInterval(timerInterval);
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+  updateDisplay();
+}
